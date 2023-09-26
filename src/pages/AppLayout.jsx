2455294e329525a88f2d5bc591d3ddc06163b88b
Hamed <jabbary.hamed@gmail.com>
2023-09-25 20:45:49 -0700

    changes

---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,13 @@
+import AppNav from "../components/AppNav"
+
+function AppLayout() {
+  return (
+    <div>
+   <AppNav />
+
+   <p>APP</p>   
+    </div>
+  )
+}
+
+export default AppLayout
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import  {Link} from 'react-router-dom';
 import PageNav from '../components/PageNav';
+import AppNav from '../components/AppNav';
 
 function Home() {
   return (
     <div>
       <PageNav />
+      <AppNav />
      <h1> World Wise </h1> 
 
-    <Link to='/pricing' >Pricing</Link>
+    <Link to='/app' >Go to the App</Link>
 
 
 
