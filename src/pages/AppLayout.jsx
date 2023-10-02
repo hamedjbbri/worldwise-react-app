3d615452b33b8f31bd6bf329fd2829b2c7d03cee
Hamed <jabbary.hamed@gmail.com>
2023-10-02 16:09:50 -0700

    change

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,13 +1,15 @@
-import AppNav from "../components/AppNav"
+import Map from "../components/Map"
+import Sidebar from "../components/Sidebar"
+import styles from './AppLayout.module.css'
+ 
 
 function AppLayout() {
   return (
-    <div>
-   <AppNav />
-
-   <p>APP</p>   
+    <div className={styles.app} >
+      <Sidebar />  
+      <Map />
     </div>
   )
 }
 
-export default AppLayout
+export default AppLayout;
